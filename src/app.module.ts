5f7c186a4b04746b0e88ba29d8dc7d56df2f5457
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-03-25 21:51:45 -0600

    db: Connection to database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import process from 'process';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      username: process.env.USERNAME,
+      password: process.env.PASS,
+      database: process.env.DATABASE,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
